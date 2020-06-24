c48aa23ce76fe17e642cb479e7c7efb5d865f5d9
POOJAKGUPTA <poojakgupta4@gmail.com>
2020-06-25 01:05:48 +0530

    added images dynamic

--- app/controllers/timings_controller.rb
+++ app/controllers/timings_controller.rb
@@ -2,4 +2,11 @@ class TimingsController < ApplicationController
 	def new
 		@timing = Timings.all
 	end
+
+	def index
+		movie_id = params[:movie]
+		theater_id =  params[:theater]
+		@movie = Movie.find(movie_id)
+		@theater = Theater.find(movie_id)
+	end
 end
